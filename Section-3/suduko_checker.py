a9024557d08f0d360a1112f95a1ac923447adee5
James Earley <jearley2@wisc.edu>
2019-07-24 11:58:26 -0500

    First pass at whole Sudoku checker using defaultdict for checking

--- Section-3/suduko_checker.py
+++ Section-3/suduko_checker.py
@@ -45,6 +45,9 @@
 # column, or sub-grid. Here we are using 0 to represent an element of
 # the Sudoku grid that the player has not yet filled in.
 
+from collections import defaultdict
+
+
 # check_sudoku should return None
 ill_formed = [[5,3,4,6,7,8,9,1,2],
               [6,7,2,1,9,5,3,4,8],
@@ -101,9 +104,13 @@ hard = [[1,0,0,0,0,7,0,9,0],
         [0,0,7,0,0,0,3,0,0]]
 
 
-NUM_ROWS = 9
-MIN_VALID_NUMBER = 0
+BOXES_IN_SET = 9
+COLS_IN_BLOCK = 3
 MAX_VALID_NUMBER = 9
+MIN_VALID_NUMBER = 0
+NUM_COLS = 9
+NUM_ROWS = 9
+ROWS_IN_BLOCK = 3
 
 
 def sanity_checks(grid):
@@ -118,45 +125,106 @@ def sanity_checks(grid):
             if not number or type(number) not int or number < MIN_VALID_NUMBER or number > MAX_VALID_NUMBER:
                 return None
 
+    return True
+
 
 def check_vertical_values(grid):
+    results = []
     col_idx = 0
-    while col_idx < NUM_ROWS:
-        unique_vals = set()
+    while col_idx < NUM_COLS:
+        values_counts = defaultdict(int)
         for row in grid:
-            unique_vals.add(row[col_idx])
+            values_counts[row[col_idx]] += 1
 
-        check_values(unique_vals)
+        results.append(check_values(values_counts))
         col_idx += 1
+
+    return all(results)  # returns True (valid) if all verticals returned True (valid), else False
 
 
 def check_horizontal_values(grid):
+    results = []
+
     for row in grid:
-        unique_vals = set(row)
-        check_values(unique_vals)
+        values_counts = defaultdict(int)
+        for val in row:
+            values_counts[val] += 1
+
+        results.append(check_values(values_counts))
+
+    return all(results)  # returns True (valid) if all horizontals returned True (valid), else False
 
 
-def check_values(values_set):
-    if len(values_set) != NUM_ROWS:
-        return False
+# todo - change to account for zero values
+def check_values(values_counts):
+    if values_dict[0] > 0:  # one or more missing values
+        return check_entries_incomplete(values_counts)
+    else:                   # all values filled
+        return check_entries_complete(values_counts)
+
+
+def check_entries_complete(values_counts):
+    for count_tuple in values_counts.items():
+        if count_tuple[1] != 1:
+            return False
 
     return True
 
 
-# todo
-def check_blocks_of_nine:
-    pass
+def check_entries_incomplete(values_counts):
+    num_boxes_filled = BOXES_IN_SET - values_counts[0]
+    total_seen = 0
+
+    for count_tuple in values_counts.items():
+        digit = values_counts[0]
+        count = values_counts[1]
+
+        if digit != 0 and count > 1:
+            return False
+        else:
+            total_seen += count
+
+    return True
+
+
+def check_blocks_of_nine(grid):
+    results = []
+
+    for row_idx in range(0, 7, 3):       # (0, 3, 6)
+        for col_idx in range(0, 7, 3):   # (0, 3, 6)
+
+            local_row_idx = 0  # will be 0 -> 2 (at this point in grid)
+            local_col_idx = 0  # will be 0 -> 2 (at this point in grid)
+            values_counts = defaultdict(int)
+
+            # add nine entries from block
+            while row_idx < ROWS_IN_BLOCK:
+                while col_idx < COLS_IN_BLOCK:
+                    val = grid[row_idx][col_idx]
+                    values_counts[val] += 1
+
+                    col_idx += 1
+
+                row_idx += 1
+
+            results.append(check_values(values_counts))
+
+    return all(results)  # returns True (valid) if all blocks of nine returned True (valid), else False
 
 
 def check_sudoku(grid):
-    sanity_checks(grid)
-    check_horizontal_values(grid)
-    check_vertical_values(grid)
+    results = []
 
-    // check groups of nine
-    # todo
+    sane = sanity_checks(grid)
+    if sane is None:
+        return None
 
-    pass
+    results.append(check_horizontal_values(grid))
+    results.append(check_vertical_values(grid))
+    results.append(check_blocks_of_nine(grid))
+
+    return all(results)  # returns True (valid) if all checks passed with True (valid), else False
+
 
 print check_sudoku(ill_formed) # --> None
 print check_sudoku(valid)      # --> True
